3a415a31874de19642b830ea19c24d5e62940775
Artem Barafanov <st117258@student.spbu.ru>
2023-12-09 15:49:59 +0300

    5

--- matematika/main.cpp
+++ matematika/main.cpp
@@ -1,45 +1,17 @@
 ﻿#include <clocale>
 #include <iostream>
+
 #include "menu.hpp"
 #include "menu_functions.hpp"
+#include "menu_items.hpp"
+
 int main() {
 	std::setlocale(LC_ALL, "");
 
-	ABar::MenuItem study_summ = { "1 - Хочу учиться складывать!", ABar::study_summ };
-	ABar::MenuItem study_substract = { "2 - Хочу учиться вычитать!", ABar::study_substract };
-	ABar::MenuItem study_multiply = { "3 - Хочу учиться умножать!", ABar::study_multiply };
-	ABar::MenuItem study_divide = { "4 - Хочу учиться делить!", ABar::study_divide };
-	ABar::MenuItem study_go_back = { "0 - Выйти в главное меню", ABar::study_go_back };
-
-	ABar::MenuItem* study_children[] = {
-		&study_go_back,
-		&study_summ,
-		&study_substract,
-		&study_multiply,
-		&study_divide,
-	};
-	const int study_size = sizeof(study_children) / sizeof(study_children[0]);
-
-	ABar::MenuItem study = { "1 - Хочу учиться математике!", ABar::show_menu, study_children, study_size };
-	ABar::MenuItem exit = { "0 - Я лучше пойду полежу...", ABar::exit };
-
-	ABar::MenuItem* main_children[] = { &exit, &study };
-	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
-
-	ABar::MenuItem main = { nullptr, ABar::show_menu, main_children, main_size };
-
-	study_summ.parent = &study;
-	study_substract.parent = &study;
-	study_multiply.parent = &study;
-	study_divide.parent = &study;
-	study_go_back.parent = &study;
-
-	study.parent = &main;
-	exit.parent = &main;
-
-	const ABar::MenuItem* current = &main;
+	const ABar::MenuItem* current = &ABar::MAIN;
 	do {
 		current = current->func(current);
 	} while (true);
+
 	return 0;
 }
---
+++ matematika/menu_items.cpp
@@ -0,0 +1,51 @@
+﻿#include "menu_items.hpp"
+
+#include <cstddef>
+
+#include "menu_functions.hpp"
+
+const ABar::MenuItem ABar::STUDY_SUMM = {
+	"1 - Хочу научиться складывать!", ABar::study_summ, &ABar::STUDY
+};
+const ABar::MenuItem ABar::STUDY_SUBSTRACT = {
+	"2 - Хочу научиться вычитать!", ABar::study_substract, &ABar::STUDY
+};
+const ABar::MenuItem ABar::STUDY_MULTIPLY = {
+	"3 - Хочу научиться умножать!", ABar::study_multiply, &ABar::STUDY
+};
+const ABar::MenuItem ABar::STUDY_DIVIDE = {
+	"4 - Хочу научиться делить!", ABar::study_divide, &ABar::STUDY
+};
+const ABar::MenuItem ABar::STUDY_GO_BACK = {
+	"0 - Выйти в главное меню", ABar::study_go_back, &ABar::STUDY
+};
+
+namespace {
+	const ABar::MenuItem* const study_children[] = {
+		&ABar::STUDY_GO_BACK,
+		&ABar::STUDY_SUMM,
+		&ABar::STUDY_SUBSTRACT,
+		&ABar::STUDY_MULTIPLY,
+		&ABar::STUDY_DIVIDE,
+	};
+	const int study_size = sizeof(study_children) / sizeof(study_children[0]);
+}
+
+const ABar::MenuItem ABar::STUDY = {
+	"1 - Хочу учиться математике!", ABar::show_menu, &ABar::MAIN, study_children, study_size
+};
+const ABar::MenuItem ABar::EXIT = {
+	"0 - Я лучше пойду полежу...", ABar::exit, &ABar::MAIN
+};
+
+namespace {
+	const ABar::MenuItem* const main_children[] = {
+		&ABar::EXIT,
+		&ABar::STUDY
+	};
+	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
+}
+
+const ABar::MenuItem ABar::MAIN = {
+	nullptr, ABar::show_menu, nullptr, main_children, main_size
+};
